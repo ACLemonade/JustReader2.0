067ef092494ea7af9776314b40e71a6e9c1ded81
Lemonade <842476697@qq.com>
2017-08-25 10:18:20 +0800

    Initial Commit

---
+++ JustReader2.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JustReader2.0
+//
+//  Created by Lemonade on 2017/8/25.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JustReader2.0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JustReader2.0
+//
+//  Created by Lemonade on 2017/8/25.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
